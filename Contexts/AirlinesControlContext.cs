4c5a96d84c732f6b798ffe4a84108ac3d03e7c03
otthonleao <otthonleao@hotmail.com>
2024-05-20 16:31:43 -0400

    fix: mundando visibilidade do construtor

--- Contexts/AirlinesControlContext.cs
+++ Contexts/AirlinesControlContext.cs
@@ -1,26 +1,35 @@
 using AirlinesControl.Entities;
+using AirlinesControl.EntityConfigurations;
 using Microsoft.EntityFrameworkCore;
 
-namespace AirlinesControl.Contexts
+namespace AirlinesControl.Contexts;
+
+public class AirlinesControlContext: DbContext
 {
-    public class AirlinesControlContext: DbContext
+    private readonly IConfiguration _configuration;
+        
+    public AirlinesControlContext(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        protected AirlinesControlContext(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+    public DbSet<Aircraft> Aircrafts => Set<Aircraft>();
+    public DbSet<Pilot> Pilots => Set<Pilot>();
+    public DbSet<Flight> Voos => Set<Flight>();
+    public DbSet<Cancellation> Cancellations => Set<Cancellation>();
+    public DbSet<Maintenance> Maintenances => Set<Maintenance>();
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AirlinesControl"));
+    }
 
-        public DbSet<Aircraft> Aircrafts => Set<Aircraft>();
-        public DbSet<Pilot> Pilots => Set<Pilot>();
-        public DbSet<Flight> Voos => Set<Flight>();
-        public DbSet<Cancellation> Cancellations => Set<Cancellation>();
-        public DbSet<Maintenance> Maintenances => Set<Maintenance>();
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AirlinesControl"));
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AircraftConfiguration());
+        modelBuilder.ApplyConfiguration(new PilotConfiguration());
+        modelBuilder.ApplyConfiguration(new FlightConfiguration());
+        modelBuilder.ApplyConfiguration(new CancellationConfiguration());
+        modelBuilder.ApplyConfiguration(new MaintenanceConfiguration());
     }
 }
