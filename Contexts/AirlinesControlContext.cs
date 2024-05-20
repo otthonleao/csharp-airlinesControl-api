59831e8ab04debb2acf7e25a860a73c9b1dc7764
otthonleao <otthonleao@hotmail.com>
2024-05-20 00:08:30 -0400

    build: setup connection with SQL Server

--- Contexts/AirlinesControlContext.cs
+++ Contexts/AirlinesControlContext.cs
@@ -9,10 +9,22 @@ namespace AirlinesControl.Contexts
 {
     public class AirlinesControlContext: DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        protected AirlinesControlContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public DbSet<Aircraft> Aircrafts => Set<Aircraft>();
         public DbSet<Pilot> Pilots => Set<Pilot>();
         public DbSet<Voo> Voos => Set<Voo>();
         public DbSet<Cancellation> Cancellations => Set<Cancellation>();
         public DbSet<Maintenance> Maintenances => Set<Maintenance>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AirlinesControl"));
+        }
     }
 }
